b66c019364aab19a275300266205b7d8d5acedea
mrsajadpp <mrsajadpp@gmail.com>
2024-12-07 23:16:18 +0530

    sql fixed

--- src/my_sql.ts
+++ src/my_sql.ts
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASS,
     database: 'tikketu'
